cb6fff634cc332e88873d126642420999efad3f6
ta.tanaka <takayahilton@gmail.com>
2019-10-31 16:29:31 +0900

    fix test.

--- src/test/scala/com/github/takayahilton/must_use/MustUsePluginSpec.scala
+++ src/test/scala/com/github/takayahilton/must_use/MustUsePluginSpec.scala
@@ -2,7 +2,7 @@ package com.github.takayahilton.must_use
 
 import java.net.URLClassLoader
 
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.{FunSuite, Matchers, OptionValues}
 
 import scala.tools.nsc.{Global, Settings}
 import scala.tools.nsc.io.VirtualDirectory
@@ -10,38 +10,44 @@ import scala.tools.nsc.reporters.ConsoleReporter
 import scala.reflect.internal.util.{BatchSourceFile, Position}
 import scala.tools.nsc.util.ClassPath
 
+class MustUsePluginSpec extends FunSuite with Matchers with OptionValues {
 
-class MustUsePluginSpec extends FunSuite with Matchers {
-  val code = "object Test { Option(1) match { case None => } }"
+  def getWarningMsg(code: String): Option[String] = {
+    var warningMsg: String = null
+    val sources = List(new BatchSourceFile("<test>", code))
 
-  val sources = List(new BatchSourceFile("<test>", code))
+    val settings = new Settings
+    val loader = getClass.getClassLoader.asInstanceOf[URLClassLoader]
+    val entries = loader.getURLs map(_.getPath)
+    // annoyingly, the Scala library is not in our classpath, so we have to add it manually
+    val sclpath = entries find(_.endsWith("scala-compiler.jar")) map(
+      _.replaceAll("scala-compiler.jar", "scala-library.jar"))
+    settings.classpath.value = ClassPath.join((entries ++ sclpath) : _*)
+    // save class files to a virtual directory in memory
+    settings.outputDirs.setSingleOutput(new VirtualDirectory("(memory)", None))
 
-  val settings = new Settings
-  val loader = getClass.getClassLoader.asInstanceOf[URLClassLoader]
-  val entries = loader.getURLs map(_.getPath)
-  // annoyingly, the Scala library is not in our classpath, so we have to add it manually
-  val sclpath = entries find(_.endsWith("scala-compiler.jar")) map(
-    _.replaceAll("scala-compiler.jar", "scala-library.jar"))
-  settings.classpath.value = ClassPath.join((entries ++ sclpath) : _*)
-  // save class files to a virtual directory in memory
-  settings.outputDirs.setSingleOutput(new VirtualDirectory("(memory)", None))
-
-  val reporter = new ConsoleReporter(settings) {
-    override def warning(pos: Position, msg: String): Unit = {
-      println("aaaaa")
-      super.warning(pos, msg)
+    val reporter = new ConsoleReporter(settings) {
+      override def warning(pos: Position, msg: String): Unit = {
+        warningMsg = msg
+      }
     }
+
+    val compiler = new Global(settings, reporter) {
+      override protected def computeInternalPhases () {
+        super.computeInternalPhases
+        for (phase <- new MustUsePlugin(this).components)
+          phasesSet += phase
+      }
+    }
+
+    new compiler.Run().compileSources(sources)
+    Option(warningMsg)
   }
 
-  val compiler = new Global(settings, reporter) {
-    override protected def computeInternalPhases () {
-      super.computeInternalPhases
-      for (phase <- new MustUsePlugin(this).components)
-        phasesSet += phase
-    }
-  }
+
 
   test("test") {
-    new compiler.Run().compileSources(sources)
+    val code = "object Test { Option(1) match { case None => } }"
+    getWarningMsg(code).value should startWith("match may not be exhaustive.")
   }
 }
